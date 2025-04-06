b675035690de89a03836d32afd75f02244c1dbc4
saidelbakai <saidelbakai1985@gmail.com>
2025-04-06 18:30:49 +0200

    Added
    GPS feature

--- test_1.py
+++ test_1.py
@@ -1,6 +1,3 @@
 print("Hello feuture 2 branch")
 print("feuture 2 done!")
-print("nex step in develop!")
-print("Hello feuture branch")
-print("First feuture done")
-print("forword en develop branch")
+
